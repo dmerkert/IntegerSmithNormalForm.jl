7a702cd9e07f30bfea81001d6d40810f3ed0706f
Dennis Merkert <git@dennis-merkert.de>
2017-05-02 22:14:33 +0200

    adss functions to negate, swap, and add rows and columns in integer matrices

--- src/IntegerSmithNormalForm.jl
+++ src/IntegerSmithNormalForm.jl
@@ -1,5 +1,119 @@
 module IntegerSmithNormalForm
 
-# package code goes here
+#export SNF
+
+"""
+    (S,B,T) = negateRow(A,i)
+
+  Negate the i-th row of the integer matrix A such that B = SAT holds true
+"""
+function negateRow(A::Array{Int,2},i::Int)
+  S = eye(Int,size(A,1))
+  T = eye(Int,size(A,2))
+  S[i,i] = -1
+  B = S*A*T
+  (S,B,T)
+end
+
+
+"""
+    (S,B,T) = negateCol(A,i)
+
+  Negate the i-th column of the integer matrix A such that B = SAT holds true
+"""
+function negateCol(A::Array{Int,2},i::Int)
+  (S,B,T) = negateRow(A',i)
+  (T',B',S')
+end
+
+
+"""
+    (S,B,T) = swapRows(A,i,j)
+
+  Negate the i-th and j-th row of the integer matrix A such that B = SAT holds true
+"""
+function swapRows(A::Array{Int,2},i::Int,j::Int)
+  S = eye(Int,size(A,1))
+  T = eye(Int,size(A,2))
+
+  S[i,i] = 0
+  S[j,j] = 0
+  S[i,j] = 1
+  S[j,i] = 1
+
+  B = S*A*T
+  (S,B,T)
+end
+
+"""
+    (S,B,T) = swapCols(A,i,j)
+
+  Negate the i-th and j-th column of the integer matrix A such that B = SAT holds true
+"""
+function swapCols(A::Array{Int,2},i::Int,j::Int)
+  (S,B,T) = swapRows(A',i,j)
+  (T',B',S')
+end
+
+
+"""
+    (S,B,T) = addRow(A,i,j,a)
+
+  Add the j-th row a times to the i-th row of the integer matrix A such that B = SAT holds true
+"""
+function addRow(A::Array{Int,2},i::Int,j::Int,a::Int)
+  S = eye(Int,size(A,1))
+  T = eye(Int,size(A,2))
+
+  S[i,j] = a
+
+  B = S*A*T
+  (S,B,T)
+end
+
+"""
+    (S,B,T) = addRow(A,i,j,a)
+
+  Add the j-th column a times to the i-th column of the integer matrix A such that B = SAT holds true
+"""
+
+function addCol(A::Array{Int,2},i::Int,j::Int,a::Int)
+  (S,B,T) = addRow(A',i,j,a)
+  (T',B',S')
+end
+
+#
+#function SNF(M::Array{Int,2})
+#  #Computes the Smith normal form according to
+#  #https://www.charite.de/sysbio/people/hoppe/Diplomarbeit_Hoppe.pdf 
+#  #Algorithmus 1
+#  size(M,1) == size(M,2) || error("SNF not implemented for rectangular
+#                                  matrices")
+#  d = size(M,1)
+#  d = 1 && return(M,eye(1),eye(1))
+#
+#  A = M
+#  S = eye(d)
+#  T = eye(d)
+#  #Ziel: A = S*M*T
+#
+#  #Find pivot unequal to zero (arbitrary element in matrix)
+#  pivot = (0,0)
+#  for i in 1:d
+#    for j in 1:d
+#      if A(i,j) != 0
+#        pivot = (i,j)
+#        break
+#      end
+#    end
+#  end
+#
+#  #Put pivot to (1,1)-position
+#  (A,S_tmp) = SNF_swapRows(1,pivot[1])
+#  (A,T_tmp) = SNF_swapCols(1,pivot[2])
+#  S = S_tmp*S
+#  T = T*T_tmp
+#
+#end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,95 @@
-using IntegerSmithNormalForm
+using IntegerSmithNormalForm: negateRow, negateCol, swapRows, swapCols, addRow, addCol
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+@testset "Negating rows and columns" begin
+    A = [[1 2];[3 4]]
+    B = [[1 2 3];[4 5 6];[7 8 9]]
+    C = [[1 2];[3 4];[5 6]]
+
+    (S,Tmp,T) = negateRow(A,1)
+    @test Tmp == [[-1 -2];[3 4]]
+    @test S*A*T == Tmp
+    @test typeof(S) == Array{Int,2}
+    @test typeof(A) == Array{Int,2}
+    @test typeof(T) == Array{Int,2}
+
+    (S,Tmp,T) = negateRow(A,2)
+    @test Tmp == [[1 2];[-3 -4]]
+    @test S*A*T == Tmp
+
+    (S,Tmp,T) = negateRow(B,3)
+    @test Tmp == [[1 2 3];[4 5 6];[-7 -8 -9]]
+    @test S*B*T == Tmp
+
+    (S,Tmp,T) = negateRow(C,2)
+    @test Tmp == [[1 2];[-3 -4];[5 6]]
+    @test S*C*T == Tmp
+
+
+    (S,Tmp,T) = negateCol(A,1)
+    @test Tmp == [[-1 2];[-3 4]]
+    @test S*A*T == Tmp
+    @test typeof(S) == Array{Int,2}
+    @test typeof(A) == Array{Int,2}
+    @test typeof(T) == Array{Int,2}
+
+    (S,Tmp,T) = negateCol(A,2)
+    @test Tmp == [[1 -2];[3 -4]]
+    @test S*A*T == Tmp
+
+    (S,Tmp,T) = negateCol(B,3)
+    @test Tmp == [[1 2 -3];[4 5 -6];[7 8 -9]]
+    @test S*B*T == Tmp
+
+    (S,Tmp,T) = negateCol(C,2)
+    @test Tmp == [[1 -2];[3 -4];[5 -6]]
+    @test S*C*T == Tmp
+end
+
+@testset "Swapping rows and columns" begin
+    A = [[1 2];[3 4];[5 6]]
+
+    (S,B,T) = swapRows(A,1,2)
+    @test B == [[3 4];[1 2];[5 6]]
+    @test S*A*T == B
+    @test typeof(S) == Array{Int,2}
+    @test typeof(A) == Array{Int,2}
+    @test typeof(T) == Array{Int,2}
+
+    (S,B,T) = swapRows(A,3,2)
+    @test B == [[1 2];[5 6];[3 4]]
+    @test S*A*T == B
+
+    A = [[1 2 3];[4 5 6]]
+    (S,B,T) = swapCols(A,1,2)
+    @test B == [[2 1 3];[5 4 6]]
+    @test S*A*T == B
+    @test typeof(S) == Array{Int,2}
+    @test typeof(A) == Array{Int,2}
+    @test typeof(T) == Array{Int,2}
+
+    (S,B,T) = swapCols(A,3,2)
+    @test B == [[1 3 2];[4 6 5]]
+    @test S*A*T == B
+end
+
+@testset "Adding rows and columns" begin
+    A = [[1 2];[3 4];[5 6]]
+
+    (S,B,T) = addRow(A,1,2,10)
+    @test B == [[31 42];[3 4];[5 6]]
+    @test S*A*T == B
+    @test typeof(S) == Array{Int,2}
+    @test typeof(A) == Array{Int,2}
+    @test typeof(T) == Array{Int,2}
+
+    A = [[1 2 3];[4 5 6]]
+
+    (S,B,T) = addCol(A,2,3,10)
+    @test B == [[1 32 3];[4 65 6]]
+    @test S*A*T == B
+    @test typeof(S) == Array{Int,2}
+    @test typeof(A) == Array{Int,2}
+    @test typeof(T) == Array{Int,2}
+
+end
