bceadb8a229bcc6da3476dbf6045991bd7263c42
Dennis Merkert <dmerkert@mathematik.uni-kl.de>
2017-05-03 19:28:02 +0200

    changes to more general typing and starts implementing SNF

--- src/IntegerSmithNormalForm.jl
+++ src/IntegerSmithNormalForm.jl
@@ -1,15 +1,15 @@
 module IntegerSmithNormalForm
 
-#export SNF
+export SNF
 
 """
     (S,B,T) = negateRow(A,i)
 
   Negate the i-th row of the integer matrix A such that B = SAT holds true
 """
-function negateRow(A::Array{Int,2},i::Int)
-  S = eye(Int,size(A,1))
-  T = eye(Int,size(A,2))
+function negateRow{I}(A::Array{I,2},i)
+  S = eye(I,size(A,1))
+  T = eye(I,size(A,2))
   S[i,i] = -1
   B = S*A*T
   (S,B,T)
@@ -21,7 +21,7 @@ end
 
   Negate the i-th column of the integer matrix A such that B = SAT holds true
 """
-function negateCol(A::Array{Int,2},i::Int)
+function negateCol{I}(A::Array{I,2},i)
   (S,B,T) = negateRow(A',i)
   (T',B',S')
 end
@@ -32,9 +32,9 @@ end
 
   Negate the i-th and j-th row of the integer matrix A such that B = SAT holds true
 """
-function swapRows(A::Array{Int,2},i::Int,j::Int)
-  S = eye(Int,size(A,1))
-  T = eye(Int,size(A,2))
+function swapRows{I}(A::Array{I,2},i,j)
+  S = eye(I,size(A,1))
+  T = eye(I,size(A,2))
 
   S[i,i] = 0
   S[j,j] = 0
@@ -50,7 +50,7 @@ end
 
   Negate the i-th and j-th column of the integer matrix A such that B = SAT holds true
 """
-function swapCols(A::Array{Int,2},i::Int,j::Int)
+function swapCols{I}(A::Array{I,2},i,j)
   (S,B,T) = swapRows(A',i,j)
   (T',B',S')
 end
@@ -61,9 +61,9 @@ end
 
   Add the j-th row a times to the i-th row of the integer matrix A such that B = SAT holds true
 """
-function addRow(A::Array{Int,2},i::Int,j::Int,a::Int)
-  S = eye(Int,size(A,1))
-  T = eye(Int,size(A,2))
+function addRow{I}(A::Array{I,2},i,j,a::I)
+  S = eye(I,size(A,1))
+  T = eye(I,size(A,2))
 
   S[i,j] = a
 
@@ -77,43 +77,73 @@ end
   Add the j-th column a times to the i-th column of the integer matrix A such that B = SAT holds true
 """
 
-function addCol(A::Array{Int,2},i::Int,j::Int,a::Int)
+function addCol{I}(A::Array{I,2},i,j,a::I)
   (S,B,T) = addRow(A',i,j,a)
   (T',B',S')
 end
 
-#
-#function SNF(M::Array{Int,2})
-#  #Computes the Smith normal form according to
-#  #https://www.charite.de/sysbio/people/hoppe/Diplomarbeit_Hoppe.pdf 
-#  #Algorithmus 1
-#  size(M,1) == size(M,2) || error("SNF not implemented for rectangular
-#                                  matrices")
-#  d = size(M,1)
-#  d = 1 && return(M,eye(1),eye(1))
-#
-#  A = M
-#  S = eye(d)
-#  T = eye(d)
-#  #Ziel: A = S*M*T
-#
-#  #Find pivot unequal to zero (arbitrary element in matrix)
-#  pivot = (0,0)
-#  for i in 1:d
-#    for j in 1:d
-#      if A(i,j) != 0
-#        pivot = (i,j)
-#        break
-#      end
-#    end
-#  end
-#
-#  #Put pivot to (1,1)-position
-#  (A,S_tmp) = SNF_swapRows(1,pivot[1])
-#  (A,T_tmp) = SNF_swapCols(1,pivot[2])
-#  S = S_tmp*S
-#  T = T*T_tmp
-#
-#end
+
+"""
+    (S,E,T) = SNF(A)
+
+  Computes the Smith normal form according to
+  https://www.charite.de/sysbio/people/hoppe/Diplomarbeit_Hoppe.pdf 
+  Algorithmus 1
+"""
+function SNF{I<:Integer}(A::Array{I,2})
+  d1 = size(A,1)
+  d2 = size(A,2)
+
+  E = A
+  S = eye(I,d1)
+  T = eye(I,d2)
+
+  for submatrixIndex in 1:min(d1,d2)
+    #Step 1: Find pivot unequal to zero (arbitrary element in sub-matrix)
+    pivot = (0,0)
+    for i in submatrixIndex:d1
+      for j in submatrixIndex:d2
+        if A[i,j] != 0
+          pivot = (i,j)
+          break
+        end
+      end
+    end
+
+    #Step 2: Put pivot to (submatrixIndex,submatrixIndex)-position
+    (S_,E,T_) = swapRows(E,submatrixIndex,pivot[1])
+    S = S_ * S
+    T = T * T_
+    (S_,E,T_) = swapCols(E,submatrixIndex,pivot[2])
+    S = S_ * S
+    T = T * T_
+
+    #check if E[submatrixIndex,submatrixIndex] divides all elements in the
+    #column and row
+    while
+      sum(
+          mod(
+              E[submatrixIndex:end,submatrixIndex],
+              E[submatrixIndex,submatrixIndex]
+             )
+         ) > 0 |
+      sum(
+          mod(
+              E[submatrixIndex,submatrixIndex:end],
+              E[submatrixIndex,submatrixIndex]
+             )
+         ) > 0
+
+    end
+
+
+
+
+
+
+
+
+  end
+end
 
 end # module
