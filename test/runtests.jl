aa926008479f67de2273d4c103e92b662d06aaf9
Dennis Merkert <git@dennis-merkert.de>
2017-05-12 08:38:28 +0200

    adjusts test to test for same type

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,9 +9,9 @@ using Base.Test
     (S,Tmp,T) = negateRow(A,1)
     @test Tmp == [[-1 -2];[3 4]]
     @test S*A*T == Tmp
-    @test typeof(S) == Array{Int,2}
-    @test typeof(A) == Array{Int,2}
-    @test typeof(T) == Array{Int,2}
+    @test typeof(S) ==   typeof(A)
+    @test typeof(Tmp) == typeof(A)
+    @test typeof(T) ==   typeof(A)
 
     (S,Tmp,T) = negateRow(A,2)
     @test Tmp == [[1 2];[-3 -4]]
@@ -29,9 +29,9 @@ using Base.Test
     (S,Tmp,T) = negateCol(A,1)
     @test Tmp == [[-1 2];[-3 4]]
     @test S*A*T == Tmp
-    @test typeof(S) == Array{Int,2}
-    @test typeof(A) == Array{Int,2}
-    @test typeof(T) == Array{Int,2}
+    @test typeof(S) ==   typeof(A)
+    @test typeof(Tmp) == typeof(A)
+    @test typeof(T) ==   typeof(A)
 
     (S,Tmp,T) = negateCol(A,2)
     @test Tmp == [[1 -2];[3 -4]]
@@ -52,9 +52,9 @@ end
     (S,B,T) = swapRows(A,1,2)
     @test B == [[3 4];[1 2];[5 6]]
     @test S*A*T == B
-    @test typeof(S) == Array{Int,2}
-    @test typeof(A) == Array{Int,2}
-    @test typeof(T) == Array{Int,2}
+    @test typeof(S) == typeof(A)
+    @test typeof(B) == typeof(A)
+    @test typeof(T) == typeof(A)
 
     (S,B,T) = swapRows(A,3,2)
     @test B == [[1 2];[5 6];[3 4]]
@@ -64,9 +64,9 @@ end
     (S,B,T) = swapCols(A,1,2)
     @test B == [[2 1 3];[5 4 6]]
     @test S*A*T == B
-    @test typeof(S) == Array{Int,2}
-    @test typeof(A) == Array{Int,2}
-    @test typeof(T) == Array{Int,2}
+    @test typeof(S) == typeof(A)
+    @test typeof(B) == typeof(A)
+    @test typeof(T) == typeof(A)
 
     (S,B,T) = swapCols(A,3,2)
     @test B == [[1 3 2];[4 6 5]]
@@ -79,17 +79,17 @@ end
     (S,B,T) = addRow(A,1,2,10)
     @test B == [[31 42];[3 4];[5 6]]
     @test S*A*T == B
-    @test typeof(S) == Array{Int,2}
-    @test typeof(A) == Array{Int,2}
-    @test typeof(T) == Array{Int,2}
+    @test typeof(S) == typeof(A)
+    @test typeof(B) == typeof(A)
+    @test typeof(T) == typeof(A)
 
     A = [[1 2 3];[4 5 6]]
 
     (S,B,T) = addCol(A,2,3,10)
     @test B == [[1 32 3];[4 65 6]]
     @test S*A*T == B
-    @test typeof(S) == Array{Int,2}
-    @test typeof(A) == Array{Int,2}
-    @test typeof(T) == Array{Int,2}
+    @test typeof(S) == typeof(A)
+    @test typeof(B) == typeof(A)
+    @test typeof(T) == typeof(A)
 
 end
